7ab0ea12b9ff79686afe9a24384a21b4e93e46a1
Qurat-ul-Ain <qurat070903@users.noreply.github.com>
2026-08-18 18:23:56 +0500

    Change in acedemics CSS

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ const storage = multer.diskStorage({
   }
 });
 
+const items = document.querySelectorAll('#academics .tl-item');
+
 const ALLOWED_TYPES = ['image/jpeg', 'image/png', 'image/webp', 'image/gif'];
 const upload = multer({
   storage,
